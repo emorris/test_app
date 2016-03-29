0ff3231a6a90acd389e4bc096eb9086271e2f880
Evan Morris <emorris@mdsol.com>
2016-03-29 13:27:36 -0700

    caching test

--- app/controllers/cache_test_controller.rb
+++ app/controllers/cache_test_controller.rb
@@ -1,9 +1,12 @@
 class CacheTestController < ApplicationController
   def cache1
-    render json: ["cache1"]
+    render json: params
   end
   def cache2
-    render json: ["cache2"]
+    @user =  Rails.cache.fetch("big_one", expires_in: 5.minutes) do 
+      User.find(params[:id])
+    end
+    render json: @user
   end
 
   def odd
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,16 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :comments, :posts
 end
 
 
-class UserShowLargeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :comment_count, :post_count
-
-  def comment_count
-      self.comments.size
-  end
-
-  def post_count
-     self.posts.size
-  end
-end
