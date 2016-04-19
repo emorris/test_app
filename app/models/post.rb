2c1feee0ad16d0cd948a83a70ba9361cdcebef93
Evan Morris <emorris@mdsol.com>
2016-04-19 10:49:05 -0700

    inverse_of

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   self.locking_column = :lock_version
-  belongs_to :user, counter_cache: true
+  belongs_to :user, counter_cache: true, inverse_of: :posts
   has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, inverse_of: :user
   has_many :comments do
     def compressed_comments
       select(:id)
